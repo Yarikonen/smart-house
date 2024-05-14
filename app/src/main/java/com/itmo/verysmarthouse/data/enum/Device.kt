3d5b6e833cf514bb6b394207434eebe898d8d8a6
Yaroslav Rudyk <y.y.rudyk@tinkoff.ru>
2024-05-14 20:08:58 +0300

    init

---
+++ app/src/main/java/com/itmo/verysmarthouse/data/enum/Device.kt
@@ -0,0 +1,17 @@
+package com.itmo.verysmarthouse.data.enum
+
+enum class DeviceType (val translation: String) {
+    LIGHT("Свет"),
+    COFFEE_MACHINE("Кофемашина"),
+    SOCKET("Розетка"),
+    DOOR_LOCK("Замок для двери"),
+    VIDECAM("Камера видеонаблюдения"),
+    ALARM_CLOCK("Будильник"),
+    BLINDS("Жалюзи"),
+    IRRIGATION_SYSTEM("Система полива"),
+    AIR_CONDITIONER("Кондиционер"),
+    MUSIC_COLUMN("Музыкальная колонка"),
+    TELEVISOR("Телевизор"),
+    ROBOT_VACUUM_CLEANER("Робот-пылесос"),
+    NONE("Другое")
+}
